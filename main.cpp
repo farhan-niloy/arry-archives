71cbaaa73fffe01b8d822cc5c71ab5bfc8a0d66f
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-04-08 13:02:29 +0600

    add some code

--- main.cpp
+++ main.cpp
@@ -18,5 +18,14 @@ int main (){
     cout<<"\nFirst vowel is: " <<vowels[sizeof(vowels)-5] <<endl;
     cout<<"\nLast vowel is: " <<vowels[sizeof(vowels)-1] <<endl;
 
+
+    double hi_temp [] {99.4, 33.4, 55.6, 77.4, 222.4};
+
+    cout<<"\nHigh temperature is: " <<hi_temp[4] <<endl;
+
+    hi_temp[4] = 99.9;
+
+    cout<<"The high temperature is now: " <<hi_temp[4];
+
     return 0;
 }
